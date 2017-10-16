d0fd234936b53e6aa81d3f6fbc54274fcf201487
Nguyen Trung <nguyentrung9311@gmail.com>
2017-10-16 15:16:46 +0700

    Update

---
+++ index.js
@@ -0,0 +1,10 @@
+const express=require("express");
+const app=express();
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.set(express.static('public'));
+
+app.get('/', (req, res) => res.render('home'));
+
+app.listen(3000, ()=>console.log("Server started!"));
